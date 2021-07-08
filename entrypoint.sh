eb7dbdbcceee5c8ab9b20ab6d143ae347f613864
Charles-P. Clermont <charles@cpclermont.com>
2021-07-08 10:23:14 -0400

    Accept CLI flags as action arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+theme_root="${INPUT_THEME_ROOT:-.}"
+flags="$INPUT_FLAGS"
+
 set -eou pipefail
 
-theme_root="${INPUT_THEME_ROOT:-.}"
-theme-check "$theme_root"
+theme-check $flags "$theme_root"
