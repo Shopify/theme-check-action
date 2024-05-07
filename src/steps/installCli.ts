a31d9588d699a7992095bb93692384d6e196b5fb
Charles-P. Clermont <charles@cpclermont.com>
2024-05-07 11:57:10 -0400

    Experimental build

--- src/steps/installCli.ts
+++ src/steps/installCli.ts
@@ -3,38 +3,37 @@ import * as semver from 'semver';
 
 const MIN_VERSION = '3.50.0';
 
-export async function installCli(version?: string) {
-  const versionSuffix = version ? `@${version}` : '';
+export async function installCli(_version?: string) {
+  const versionSuffix = '@0.0.0-experimental-20240507155341'
 
-  if (!isValidVersion(version)) {
-    throw new Error(
-      `Shopify CLI version: ${version} is invalid or smaller than ${MIN_VERSION}`,
-    );
-  }
+  // if (!isValidVersion(version)) {
+  //   throw new Error(
+  //     `Shopify CLI version: ${version} is invalid or smaller than ${MIN_VERSION}`,
+  //   );
+  // }
 
   await exec('npm', [
     'install',
     '--no-package-lock',
     '--no-save',
     '@shopify/cli' + versionSuffix,
-    '@shopify/theme' + versionSuffix,
   ]);
 }
 
-function isValidVersion(version?: string): boolean {
-  if (!version) {
-    return true;
-  }
-
-  // Check if the version is valid
-  if (!semver.valid(version)) {
-    return false;
-  }
-
-  // Check if the version is greater than or equal to the minimum version
-  if (!semver.gte(version, MIN_VERSION)) {
-    return false;
-  }
-
-  return true;
-}
+// function isValidVersion(version?: string): boolean {
+//   if (!version) {
+//     return true;
+//   }
+//
+//   // Check if the version is valid
+//   if (!semver.valid(version)) {
+//     return false;
+//   }
+//
+//   // Check if the version is greater than or equal to the minimum version
+//   if (!semver.gte(version, MIN_VERSION)) {
+//     return false;
+//   }
+//
+//   return true;
+// }
