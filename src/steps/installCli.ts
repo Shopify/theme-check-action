254604799dbcc6bb9cb125838346f8fe8cfa4518
Charles-P. Clermont <charles@cpclermont.com>
2024-05-10 14:20:45 -0400

    Don't install `@shopify/theme` from 3.59 onward

--- src/steps/installCli.ts
+++ src/steps/installCli.ts
@@ -2,38 +2,64 @@ import { exec } from '@actions/exec';
 import * as semver from 'semver';
 
 const MIN_VERSION = '3.50.0';
+const MAX_THEME_VERSION = '3.59.0';
 
-export async function installCli(_version?: string) {
-  const versionSuffix = '@0.0.0-experimental-20240507155341'
+export async function installCli(version?: string) {
+  const versionSuffix = version ? `@${version}` : '';
 
-  // if (!isValidVersion(version)) {
-  //   throw new Error(
-  //     `Shopify CLI version: ${version} is invalid or smaller than ${MIN_VERSION}`,
-  //   );
-  // }
+  if (!isValidVersion(version)) {
+    throw new Error(
+      `Shopify CLI version: ${version} is invalid or smaller than ${MIN_VERSION}`,
+    );
+  }
 
-  await exec('npm', [
-    'install',
-    '--no-package-lock',
-    '--no-save',
-    '@shopify/cli' + versionSuffix,
-  ]);
+  await exec(
+    'npm',
+    [
+      'install',
+      '--no-package-lock',
+      '--no-save',
+      `@shopify/cli${versionSuffix}`,
+      shouldIncludeTheme(version)
+        ? `@shopify/theme${versionSuffix}`
+        : '',
+    ].filter(Boolean),
+  );
 }
 
-// function isValidVersion(version?: string): boolean {
-//   if (!version) {
-//     return true;
-//   }
-//
-//   // Check if the version is valid
-//   if (!semver.valid(version)) {
-//     return false;
-//   }
-//
-//   // Check if the version is greater than or equal to the minimum version
-//   if (!semver.gte(version, MIN_VERSION)) {
-//     return false;
-//   }
-//
-//   return true;
-// }
+function shouldIncludeTheme(version?: string) {
+  if (!version || version.includes('experimental')) {
+    return false;
+  }
+
+  if (!semver.valid(version)) {
+    return false;
+  }
+
+  if (
+    semver.gte(version, MIN_VERSION) &&
+    semver.lt(version, MAX_THEME_VERSION)
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
+function isValidVersion(version?: string): boolean {
+  if (!version || version.includes('experimental')) {
+    return true;
+  }
+
+  // Check if the version is valid
+  if (!semver.valid(version)) {
+    return false;
+  }
+
+  // Check if the version is greater than or equal to the minimum version
+  if (!semver.gte(version, MIN_VERSION)) {
+    return false;
+  }
+
+  return true;
+}
