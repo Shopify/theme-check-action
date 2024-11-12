a1df2d1021a277e3f3a2167cd187aefd6488d769
Anders Søgaard <9662430+andershagbard@users.noreply.github.com>
2024-11-12 16:02:44 +0100

    Revert

--- src/addAnnotations.ts
+++ src/addAnnotations.ts
@@ -179,7 +179,7 @@ export async function addAnnotations(
     ...ctx.repo,
     check_run_id: check.data.id,
     name: CHECK_NAME,
-    conclusion: 'success',
+    conclusion: exitCode > 0 ? 'failure' : 'success',
     output: {
       title: CHECK_NAME,
       summary: `${errorCount} error(s), ${warningCount} warning(s) found`,
